b695020fe153e3fc55e27cf30848a9143cf40c2e
shindaehyun <qqwwee08@naver.com>
2022-01-08 19:08:16 +0900

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  plugins: ["@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+    "import/resolver": {
+      alias: {
+        map: [["@src", "./src"]],
+      },
+    },
+  },
+  // Fine tune rules
+  rules: {
+    "@typescript-eslint/no-var-requires": 0,
+  },
+};
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+import TouchDrawer from "./TouchDrawer";
+
+export default TouchDrawer;
+// ReactDOM.render(
+//     <TouchDrawer isVisible onClose={() => null}>asd</TouchDrawer>,
+//     document.getElementById('root')
+// );
+//
