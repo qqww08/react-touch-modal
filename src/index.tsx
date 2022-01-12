99e34a4ca0b4c8dbd89588aaaacc8de4c86cb11a
Dan <qqwwee08@naver.com>
2022-01-12 23:13:15 +0900

    remove index.tsx

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ReactKeypad from "./lib/ReactKeypad";
-
-ReactDOM.render(
-  <ReactKeypad isVisible onClose={() => null} onFinish={(password) => console.log(password)} />,
-  document.getElementById("root")
-);
